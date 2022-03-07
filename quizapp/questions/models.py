9a91da6963652be8577bec011c46cce0fb7c27e0
Aliaksandr <aadvorink@yandex.ru>
2022-03-08 02:01:43 +0300

    added new two models

--- quizapp/questions/models.py
+++ quizapp/questions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from quizapp.quizzes.models import Quiz
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+    def get_answer(self):
+        return self.answer_set.all()
+
+
+# set answers class
--- quizapp/quizzes/models.py
+++ quizapp/quizzes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+DIFF_CHOICE = [
+    ('easy', 'easy'),
+    ('medium', 'medium'),
+    ('hard', 'hard'),
+]
+
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=150)
+    topic = models.CharField(max_length=150)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text='duration of the quiz')
+    score = models.IntegerField(help_text='required score to pass the quiz')
+    difficulty = models.CharField(max_length=50, choices=DIFF_CHOICE)
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+
+    def get_question(self):
+        return self.question_set.all()
