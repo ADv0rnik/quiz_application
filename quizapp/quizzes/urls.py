924e7f4dd4d078791ac0e737767a184b8ad0ebd8
Aliaksandr <aadvorink@yandex.ru>
2022-03-16 11:30:33 +0300

    Updated JS and html

--- quizapp/quizzes/urls.py
+++ quizapp/quizzes/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import (
-    QuizListView,
     quiz,
     quiz_data,
     quiz_data_view,
+    save_quiz_data,
 )
 
 urlpatterns = [
     path('', quiz, name='quiz'),
     path('<int:pk>/', quiz_data, name='quiz-data'),
     path('<int:pk>/data/', quiz_data_view, name='quiz-data-view'),
+    path('<int:pk>/save/', save_quiz_data, name='save-data'),
 ]
--- quizapp/quizzes/views.py
+++ quizapp/quizzes/views.py
@@ -1,12 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from questions.models import Question, Answer
 from quizzes.models import Quiz
-
-
-class QuizListView(ListView):
-    model = Quiz
-    template_name = 'quizzes.html'
+from results.models import Results
 
 
 def quiz(request):
@@ -34,8 +31,53 @@ def quiz_data_view(request, pk):
         questions.append({str(q): answers})
     return JsonResponse({
         'data': questions,
-        'time': data.time,
     })
+
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+
+@login_required
+def save_quiz_data(request, pk):
+    if is_ajax(request=request):
+        questions = []
+        data = request.POST
+        data_ = dict(data.lists())
+        data_.pop('csrfmiddlewaretoken')
+
+        for key in data_.keys():
+            question = Question.objects.get(text=key)
+            questions.append(question)
+
+        user = request.user
+        quiz_ = Quiz.objects.get(pk=pk)
+        score = 0
+        c = 100 / quiz_.number_of_questions  # coefficient that define a value to recalculate the score
+        results = []
+        correct_answer = None
+
+        # Checking for correct answers
+        for q_ in questions:
+            answer_selected = request.POST.get(q_.text)
+            if answer_selected != "":
+                question_answers = Answer.objects.filter(question=q_)
+                for answer in question_answers:
+                    if answer_selected == answer.text:
+                        score += 1
+                        correct_answer = answer.text
+                    else:
+                        correct_answer = answer.text
+                results.append({str(q_): {'correct answer': correct_answer, 'answered': answer_selected}})
+            else:
+                results.append({str(q_): 'missed'})
+
+        score_ = score * c
+        Results.objects.create(quiz=quiz_, user=user, score=score_)
+        if score_ >= quiz_.score:
+            return JsonResponse({'passed': True, 'score': score_, "results": results})
+        else:
+            return JsonResponse({'passed': False, 'score': score_, "results": results})
 
 
 def index(request):
