dba9068bdbcc6a6d6a7d5f49c19fa714cebc7f57
Alex Dvornik <aadvornik@gmail.com>
2022-04-17 02:16:53 +0300

    Updated

--- quizapp/profile/views.py
+++ quizapp/profile/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+from django.contrib import messages
 
 
 def register_user(request):
     form = CreateUserForm()
-    context = {'form': form}
 
     if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
 
+            return redirect('login')
+    context = {'form': form}
     return render(request, 'register.html', context)
 
 
