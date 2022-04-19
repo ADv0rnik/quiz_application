8a29b92f089cf551e29aad4c506fce0a1300d4f2
Alex Dvornik <aadvornik@gmail.com>
2022-04-20 00:45:28 +0300

    Updated files

--- quizapp/profile/forms.py
+++ quizapp/profile/forms.py
@@ -6,4 +6,4 @@ from django.contrib.auth.forms import UserCreationForm
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
--- quizapp/profile/views.py
+++ quizapp/profile/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import CreateUserForm
 from django.contrib import messages
 
+from .models import Student
+
 
 def register_user(request):
     form = CreateUserForm()
@@ -11,9 +13,19 @@ def register_user(request):
     if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            email = form.cleaned_data.get('email')
+
+            Student.objects.create(
+                user=user,
+                first_name=first_name,
+                last_name=last_name,
+                mail=email,
+            )
+            messages.success(request, 'Account was created for ' + username)
 
             return redirect('login')
     context = {'form': form}
@@ -24,7 +36,6 @@ def login_user(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
-
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
