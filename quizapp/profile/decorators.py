86c012bae916f90e8524e5235c0de8bd63dd59ef
Alex Dvornik <aadvornik@gmail.com>
2022-04-28 12:10:31 +0300

    Updated decorator

--- quizapp/profile/decorators.py
+++ quizapp/profile/decorators.py
@@ -5,8 +5,7 @@ from django.shortcuts import redirect
 def unauthenticated_user(view_function):
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated:
-            username = request.user.username
-            return redirect('user', username=username)
+            return redirect('home')
         else:
             return view_function(request, *args, **kwargs)
     return wrapper
@@ -18,10 +17,25 @@ def allowed_user(allowed_roles=[]):
             group = None
             if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
-
             if group in allowed_roles:
+                print(f'Allowed. You entered as {group}')
                 return view_func(request, *args, **kwargs)
             else:
+                print(f'Not allowed. You are {group}')
                 return HttpResponse('You are not authorized to view this page')
         return wrapper
     return decorator
+
+
+def supervisor_only(view_function):
+    def wrapper(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'admin':
+            print('you are supervisor')
+            return view_function(request, *args, **kwargs)
+        else:
+            print('you are not supervisor')
+            return redirect('user', username=request.user)
+    return wrapper
